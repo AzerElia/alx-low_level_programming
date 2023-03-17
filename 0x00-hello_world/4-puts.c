1378cbb6effeb14c7bc126aa128824a8e9adaaa1
AzerElia <azerelia@outlook.com>
2023-03-17 07:45:13 -0700

    Hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+  *main - main entry point
+  *return: always 0 (Success)
+  */
+int main(void)
+{
+	puts("\"programming is like buliding a multilingual puzzel");
+	return (0);
+}
